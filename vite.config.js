ca643ebcbb34bdf808f75540341953197eaaa433
uniflora <welchs42@gmail.com>
2025-12-22 18:12:04 +0900

    AA
    -

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/ttmud/',  // GitHub Pages 배포용 (레포지토리 이름)
   server: {
     port: 3000,
     open: true
